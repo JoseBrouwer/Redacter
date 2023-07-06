03ad9312a56452b770c05a70d0c19af79f09dd9d
JoseBrouwer <jbb20b@fsu.edu>
2023-07-06 16:55:44 -0400

    Redacts pro_nouns, stings & substrings

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Redacter
 Text Redacter
+This is a simple text redaction program that takes a text file as input and replaces key words with a redacted version.
+The program takes 4 arguments: the input file, the output file, the word to redact, and the redacted word.
+The program will replace the word to redact with the redacted word in the output file.
+If the word to redact is pro_nouns, then the program will redact all pronouns in the input file.
+The program will also redact the word to redact if it is a substring of a word in the input file. Want it to just be whole words.
+Currently creates a new file but this is for testing purposes and will be changed to overwrite the input file
--- TextRedacter.py
+++ TextRedacter.py
@@ -1,7 +1,5 @@
 import sys
 
-# This is a simple text redaction program that takes a text file as input and replaces key words with a redacted version.
-
 """
 This function counts the number of X's to be used in the redacted word. The pattern follows that if the word has 5 letters or less, then the redacted word will have 1 X. If the word has 6-10 letters, then the redacted word will have 2 X's. If the word has 11-15 letters, then the redacted word will have 3 X's. If the word has 16-20 letters, then the redacted word will have 4 X's. If the word has 21 or more letters, then the redacted word will have 5 X's.
 """
@@ -26,19 +24,33 @@ def count_X(string):
 #python TextRedacter.py <input_file> <output_file> <word_to_redact> <redacted_word>
 # Check for correct number of arguments
 def Redact(input_file, output_file, word_to_redact):
-    if (len(sys.argv) != 4):
-        print("Incorrect number of arguments. Please enter 4 arguments.")
-    else:
-        file = open(input_file, "r")
-        output = open(output_file, "w")
+    file = open(input_file, "r")
+    output = open(output_file, "w")
+    if (word_to_redact == 'pro_nouns'): #redact all pronouns
+        for line in file:
+            for word in line.split():  #go word by word
+                if word in pro_nouns:
+                    line = line.replace(word, "X" * count_X(word))
+            output.write(line)
+    else: #redact a specific word
         redacted_word = "X" * count_X(word_to_redact)
         for line in file:
             if (word_to_redact in line):
                 output.write(line.replace(word_to_redact, redacted_word))
             else:
                 output.write(line)
-        file.close()
-        output.close()
+    file.close()
+    output.close()
 
-Redact(sys.argv[1], sys.argv[2], sys.argv[3])
+pro_nouns = ['she', 'She', 'Her','her', 'Hers', 'hers', 'He', 'he', 'Him', 'him', 'His', 'his'] #list of pronouns
+if ((len(sys.argv) == 2) and (sys.argv[1] == '-help')):
+    print("\n-----------------------------------------------------------------------------------------------------------------------------------------------\n")
+    print("Sample use: python TextRedacter.py <input_file> <output_file> <word_to_redact>\n\nReplace <input_file> with the name of the file you want to redact.\n\nReplace <output_file> with the name of the file you want to write the redacted text to.\n\nReplace <word_to_redact> with the word you want to redact.\n\nNote: You can pass pro_nouns as the <word_to_redact> to redact all pronouns.\nExample: python TextRedacter.py input.txt output.txt pro_nouns")
+    print("\n-----------------------------------------------------------------------------------------------------------------------------------------------\n")
+elif (len(sys.argv) != 4):
+    print("\n-----------------------------------------------------------------------------------------------------------------------------------------------\n")
+    print("Incorrect number of arguments. Please enter 4 arguments.\n\nSample use: python TextRedacter.py <input_file> <output_file> <word_to_redact>\n\nHelp: python TextRedacter.py -help")
+    print("\n-----------------------------------------------------------------------------------------------------------------------------------------------\n")
+else:
+    Redact(sys.argv[1], sys.argv[2], sys.argv[3])
 
